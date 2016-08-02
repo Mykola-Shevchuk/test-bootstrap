970aa0f3ea1286801a369462d650683093844e09
Mykola-Shevchuk <mykola.shevchuk.ua@gmail.com>
2016-08-02 23:08:48 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
-jQuery(document).ready(function($) {
-	
+// SLIDER
+
+$(document).ready(function($) {
+
+	//configuration
+	var width = 720;
+	var	animationSpeed = 1000;
+	var currentSlide = 1;
+	var position = 0;
+
+	// cache DOM
+	var $slider = $('#slider');
+	var $slideContainer = $slider.find('.slides');
+	var $slides = $slideContainer.find('.slide');
+
+	// navigation
+	var $left = $('.left-arrow');
+	var $right = $('.right-arrow');
+
+
+	function nav() {
+		$right.stop().on('click', function() {
+			position = Math.max(position - width * currentSlide, -width * ($slides.length - currentSlide));
+
+			$slideContainer.stop().animate({
+				marginLeft: position + 'px' 
+			}, animationSpeed, function() {
+
+			});
+		});
+
+		$left.stop().on('click', function() {
+			position = Math.min(position + width * currentSlide, 0)
+
+			$slideContainer.stop().animate({
+				marginLeft:  position + 'px'
+			}, animationSpeed, function() {
+
+			});
+		});
+	}
+
+	nav();
 });
